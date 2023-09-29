4d256627bc39d20d549da0a46d187253b4860972
Rohit Sarkar <rohitsarkar034@gmail.com>
2023-09-29 16:47:40 +0530

    Initial commit after using the Spring Initializr and using: Dependency manager->Maven, Language->Java 11, Spring-boot version:2.7.16 and Dependency used->Spring Web and changing server.port from default to 7000

---
+++ src/main/java/com/example/Menu/JAVA/Project/MenuJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Menu.JAVA.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Menu/JAVA/Project/MenuJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Menu.JAVA.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
