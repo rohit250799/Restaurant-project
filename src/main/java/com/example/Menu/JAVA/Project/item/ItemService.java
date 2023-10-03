8f6d5c658ab0e92703eb8b382a3d01a850e4657f
Rohit Sarkar <rohitsarkar034@gmail.com>
2023-10-03 08:06:21 +0530

    1. Implementing the finaAll(), find, create, update and delete method in ItemService class. 2. Creating a list of end-points that we will need. 3. Creating an ItemController class inside the item package & importing the RestController and RequestMapping interfaces. 4. Creating the findAll, find and create methods and their bocies in ItemController class.

--- src/main/java/com/example/Menu/JAVA/Project/item/ItemService.java
+++ src/main/java/com/example/Menu/JAVA/Project/item/ItemService.java
@@ -4,7 +4,10 @@ import org.springframework.data.map.repository.config.EnableMapRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 @Service
 @EnableMapRepositories
 public class ItemService {
@@ -23,7 +26,36 @@ public class ItemService {
     }
 
     public List<Item> findAll(){
-
+        List<Item> list = new ArrayList<>();
+        Iterable<Item> items = repository.findAll();
+        items.forEach(list::add);
+        return list;
     }
 
+    public Optional<Item> find(long id){
+        return repository.findById(id);
+    }
+
+    public Item create(Item item){
+        Item copy = new Item(
+                new Date().getTime(),
+                item.getName(),
+                item.getPrice(),
+                item.getDescription(),
+                item.getImage()
+        );
+        return repository.save(copy);
+    }
+
+    public Optional<Item> update(long id, Item newItem){
+        return repository.findById(id)
+                .map(oldItem -> {
+                    Item updated = oldItem.updateWith(newItem);
+                    return repository.save(updated);
+                });
+    }
+
+    public void delete(long id){
+        repository.deleteById(id);
+    }
 }
