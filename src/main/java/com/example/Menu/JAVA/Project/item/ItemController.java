a90eae3b5c4777fe6adf1a928d11c800a9b2fb98
Rohit Sarkar <rohitsarkar034@gmail.com>
2023-10-09 22:03:51 +0530

    After the previous commit, if incorrect data is passed, 400 bad request is returned but no specific message and Spring throws a MethodArgumentNotValidException. 1. In the ItemController class, defining an exception handler and implementing it. 2. Importing FieldError, ObjectError, MethodArgumentNotValidException, Hashmap, Map and Valid in ItemController class and using them. 3. The implementation of point 3 produces correct error log messages now.

--- src/main/java/com/example/Menu/JAVA/Project/item/ItemController.java
+++ src/main/java/com/example/Menu/JAVA/Project/item/ItemController.java
@@ -1,35 +1,43 @@
 package com.example.Menu.JAVA.Project.item;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.lang.reflect.Field;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 @RestController
 @RequestMapping("api/menu/items") //this line ensures all endpoints share same resource path
 public class ItemController {
     private final ItemService service;
 
-    public ItemController(ItemService service){
+    public ItemController(ItemService service) {
         this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<List<Item>> findAll(){
+    public ResponseEntity<List<Item>> findAll() {
         List<Item> items = service.findAll();
         return ResponseEntity.ok().body(items);
     }
 
     @GetMapping("/{id}") //line denotes GET /api/menu/items/{id} path
-    public ResponseEntity<Item> find(@PathVariable("id") Long id){ //to get id value, using annotation
+    public ResponseEntity<Item> find(@PathVariable("id") Long id) { //to get id value, using annotation
         Optional<Item> item = service.find(id); //if we find item with specific id, returning in a response body else 404 response
         return ResponseEntity.of(item);
     }
 
     @PostMapping
-    public ResponseEntity<Item> create (@RequestBody Item item){
+    public ResponseEntity<Item> create(@Valid @RequestBody Item item) {
         Item created = service.create(item);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
@@ -40,8 +48,8 @@ public class ItemController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Item> update(
-            @PathVariable("id") Long id,
-            @RequestBody Item updatedItem) {
+            @PathVariable("id") Long id, @Valid
+    @RequestBody Item updatedItem) {
 
         Optional<Item> updated = service.update(id, updatedItem);
 
@@ -59,8 +67,22 @@ public class ItemController {
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Item> delete(@PathVariable("id") Long id){
+    public ResponseEntity<Item> delete(@PathVariable("id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach((error) -> {
+            String key = ((FieldError) error).getField();
+            String value = error.getDefaultMessage();
+            map.put(key, value);
+        });
+
+        return ResponseEntity.badRequest().body(map);
+    }
 }
+
